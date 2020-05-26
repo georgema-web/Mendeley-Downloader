0a3d0a7b9238296c69d13f3e4108b03afec3765d
georgecma <georgema689@gmail.com>
2020-05-26 17:03:08 -0400

    hello world

---
+++ app.py
@@ -0,0 +1,17 @@
+
+
+
+import flask
+
+
+app = flask.Flask(__name__)
+
+@app.route ('/')
+def index ():
+
+    return flask.render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run() 
